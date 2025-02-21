bb9443c5500edd16aed77eb9fd3918cdc87c0596
Tim Harding <Tim@TimHarding.co>
2025-02-21 14:33:14 -0800

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::sync::Arc;
 
+use axum::body::Body;
+use axum::http::StatusCode;
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
@@ -90,13 +95,40 @@ fn main() {
 async fn main_async() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
+    let mut client_for_axum = None;
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    let (service, socket) = LspService::new(|client| {
+        client_for_axum = Some(Arc::new(client.clone()));
+        Backend { client }
+    });
+
+    let app = Router::new().route("/", post(handle_post));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     let file = OpenOptions::new()
         .write(true)
         .truncate(true)
         .open("/home/tim/Documents/temp/log.txt")
         .unwrap();
     drop(file);
-    Server::new(stdin, stdout, socket).serve(service).await;
+
+    let axum_handle = tokio::spawn(async {
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    let lsp_handle = tokio::spawn(async {
+        Server::new(stdin, stdout, socket).serve(service).await;
+    });
+
+    axum_handle.await.unwrap();
+    lsp_handle.await.unwrap();
+}
+
+#[derive(serde::Deserialize)]
+enum EditMessage {
+    Changed(String),
+}
+
+async fn handle_post(body: Json<EditMessage>) -> StatusCode {
+    StatusCode::OK
 }
